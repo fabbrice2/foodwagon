18c932723e69a13b250ec42eb2c5bafb104a78d9
fabrice <fabriceamenouglo@gmail.com>
2023-06-26 19:26:26 +0200

    work in progress

--- foodwagon.js
+++ foodwagon.js
@@ -1,7 +1,10 @@
-const foodImages = document.querySelectorAll('.foodimg');
+const $foodImages = document.querySelectorAll('.foodimg');
+// const $bgImages = document.querySelectorAll('.bgimg');
+// const $nextbtn =  document.querySelector('.arrow.next')
+// const $backbtn =  document.querySelector('.arrow.back')
+// const $slide = document.querySelectorAll('.slide_item')
 
-
-const imageUrls = [
+const $imageUrls = [
   'images/burger-de-luxe.jpg',
   'images/Image.png',
   'images/Image2.png',
@@ -9,13 +12,52 @@ const imageUrls = [
 ];
 
 
-foodImages.forEach((foodImg, index) => {
-    if (index < imageUrls.length) {
-      const imageUrl = imageUrls[index];
-      foodImg.style.backgroundImage = `url(${imageUrl})`;
+$foodImages.forEach((foodImg, index) => {
+    if (index < $imageUrls.length) {
+      const $imageUrl = $imageUrls[index];
+      foodImg.style.backgroundImage = `url(${$imageUrl})`;
     }
-  });
+});
 
 
+const $backgroundImages = document.querySelectorAll('.bgimg')
 
+const $backgroundImagesUrls = [
+    'images/foodworld.png',
+    'images/Donutshut.png',
+    'images/Pizzahub.png',
+    'images/subway.png',
+    'images/taco.png',
+    'images/ruby.png',
+    'images/red.png',
+    'images/kuata.png'
+];
 
+$backgroundImages.forEach((bgImg, index) => {
+  if (index < $backgroundImagesUrls.length) {
+    const $backgroundImagesUrl = $backgroundImagesUrls[index];
+    bgImg.style.backgroundImage = `url(${$backgroundImagesUrl})`;
+  }
+});
+
+  // const bgImageUrls = [
+  //   'images/foodworld.png',
+  //   'images/Donuts hut.png',
+  //   'images/Pizzahub.png',
+  //   'images/subway.png',
+  //   'images/taco.png',
+  //   'images/ruby.png',
+  //   'images/red.png',
+  //   'images/kuata.png'
+  // ];
+  
+  // const bgImages = document.querySelectorAll('.bgimg');
+  
+  // bgImages.forEach((bgImg, i) => {
+  //   if (i < bgImageUrls.length) {
+  //     const bgImageUrl = bgImageUrls[i];
+  //     bgImg.style.backgroundImage = `url("${bgImageUrl}")`;
+  //   }
+  // });
+  
+  
